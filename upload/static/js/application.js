e12f5146588836e2ce33a5ead9ab1f50463e71ec
Darren Riedlinger <darrenriedlinger@gmail.com>
2013-02-06 09:34:56 -0800

    Moved multiuploader scripts and templates to external {% includes %}

--- upload/static/js/application.js
+++ upload/static/js/application.js
@@ -1,3 +1,4 @@
+// TODO: programatically incorporate options (i.e maxfilesize) via cookies?
 /*
  * jQuery File Upload Plugin JS Example 5.0.2
  * https://github.com/blueimp/jQuery-File-Upload
@@ -12,31 +13,43 @@
 /*jslint nomen: true */
 /*global $ */
 
-$(function () {
-    'use strict';
+$(function() {
+	'use strict';
+	// Replace non-js link with the multifile upload form
+	$('#upload_link').replaceWith($('#fileupload'))
+	$('#fileupload').show()
 
-    // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload();
+	// initialize the jquery file upload widget
+	// for a complete option reference go to
+	// https://github.com/blueimp/jquery-file-upload
+	$('#fileupload').fileupload({
+		// this formdata is neccessary to pass the csrf and pass uid to django
+		formdata: [
+	{ name: "csrfmiddlewaretoken", value: $('input[name=csrfmiddlewaretoken]').val() },
+		],
+	//	maxFileSize: {{ maxfilesize }},
+	//	minFileSize: {{ minfilesize }},
+		sequentialUploads: true
+	});
 
-    // Load existing files:
-    $.getJSON($('#fileupload form').prop('action'), function (files) {
-        var fu = $('#fileupload').data('fileupload');
-        fu._adjustMaxNumberOfFiles(-files.length);
-        fu._renderDownload(files)
-            .appendTo($('#fileupload .files'))
-            .fadeIn(function () {
-                // Fix for IE7 and lower:
-                $(this).show();
-            });
-    });
-
-    // Open download dialogs via iframes,
-    // to prevent aborting current uploads:
-    $('#fileupload .files a:not([target^=_blank])').live('click', function (e) {
-        e.preventDefault();
-        $('<iframe style="display:none;"></iframe>')
-            .prop('src', this.href)
-            .appendTo('body');
-    });
-
+	// Load existing files
+	$.getJSON($('#fileupload form').prop('action'), function (files) {
+		var fu = $('#fileupload').data('fileupload');
+		fu._adjustMaxNumberOfFiles(-files.length);
+		fu._renderDownload(files)
+		.appendTo($('#fileupload .files'))
+		.fadeIn(function () {
+			// Fix for IE7 and lower:
+			$(this).show();
+		});
+	});
+	
+	// open download dialogs via iframes,
+	// to prevent aborting current uploads
+	$('#fileupload .files a:not([target^=_blank])').live('click', function (e) {
+		e.preventDefault();
+		$('<iframe style="display:none;"></iframe>')
+		.prop('src', this.href)
+		.appendTo('body');
+	});
 });
