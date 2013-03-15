b16cb3fd8451277906b0792accd5e816155cca4d
Darren Riedlinger <darrenriedlinger@gmail.com>
2013-03-15 11:36:12 -0700

    Created TableSelectMultiple Widget Modified upload.views.py to utilize this widget

---
+++ upload/widgets.py
@@ -0,0 +1,75 @@
+from django.forms import CheckboxInput, SelectMultiple
+from django.utils.encoding import force_unicode
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+class TableSelectMultiple(SelectMultiple):
+    """
+    Provides selection of items via checkboxes, with a table row
+    being rendered for each item, the first cell in which contains the
+    checkbox.
+
+    When providing choices for this field, give the item as the second
+    item in all choice tuples. For example, where you might have
+    previously used::
+
+        field.choices = [(item.id, item.name) for item in item_list]
+
+    ...you should use::
+
+        field.choices = [(item.id, item) for item in item_list]
+
+    Credit:insin http://djangosnippets.org/snippets/518/
+    Adapted item_attrs to take a list of (label, attr) tuples with label rendered
+    in the <th> row.
+    Adapted __init__ to take an optional label for the checkbox column
+    Use conditional_escape instead of escape (allow html item_attrs)
+    """
+    def __init__(self, item_attrs, checkbox_label='', *args, **kwargs):
+        """
+        item_attrs
+            Defines the attributes of each item which will be displayed
+            as a column in each table row, in the order given.
+
+            Any callables in item_attrs will be called with the item to be
+            displayed as the sole parameter.
+
+            Any callable attribute names specified will be called and have
+            their return value used for display.
+
+            All attribute values will be escaped.
+        """
+        super(TableSelectMultiple, self).__init__(*args, **kwargs)
+        self.item_attrs = item_attrs
+        self.checkbox_label = checkbox_label
+
+    def render(self, name, value, attrs=None, choices=()):
+        if value is None: value = []
+        has_id = attrs and 'id' in attrs
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [u'<table class="existing_objects_list">']
+        str_values = set([force_unicode(v) for v in value]) # Normalize to strings.
+        output.append(u'<tr><th>%s</th>' % conditional_escape(self.checkbox_label))
+        for label, attr in self.item_attrs:
+            output.append(u'<th>%s</th>' % conditional_escape(label))
+        output.append(u'</tr>')
+        for i, (option_value, item) in enumerate(self.choices):
+            # If an ID attribute was given, add a numeric index as a suffix,
+            # so that the checkboxes don't all have the same ID attribute.
+            if has_id:
+                final_attrs = dict(final_attrs, id='%s_%s' % (attrs['id'], i))
+            cb = CheckboxInput(final_attrs, check_test=lambda value: value in str_values)
+            option_value = force_unicode(option_value)
+            rendered_cb = cb.render(name, option_value)
+            output.append(u'<tr><td>%s</td>' % rendered_cb)
+            for label, attr in self.item_attrs:
+                if callable(attr):
+                    content = attr(item)
+                elif callable(getattr(item, attr)):
+                    content = getattr(item, attr)()
+                else:
+                    content = getattr(item, attr)
+                output.append(u'<td>%s</td>' % conditional_escape(content))
+            output.append(u'</tr>')
+        output.append(u'</table>')
+        return mark_safe(u'\n'.join(output))
