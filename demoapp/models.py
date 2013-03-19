47a03491e3c124bd7bf61f72767cf9b20e005296
Darren Riedlinger <darrenriedlinger@gmail.com>
2013-03-19 03:17:47 -0700

    Deleted regular m2mfield from demomodel

--- demoapp/models.py
+++ demoapp/models.py
@@ -6,7 +6,6 @@ from django.core.urlresolvers import reverse
 class DemoModel(models.Model):
     sometext = models.CharField(max_length=30)
     attachments = FileSetField('upload.File', related_name='+')
-    foo = models.ManyToManyField('upload.File')
 
     def get_absolute_url(self):
         return reverse('crud_detail', args=[self.pk])
